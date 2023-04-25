192a26d5481bd6587e635a643cbc53fd071342c0
Grigory Korbut <grisga2001@gmail.com>
2023-04-25 16:50:04 +0300

    Create solution.ts

---
+++ medium/replace-all/solution.ts
@@ -0,0 +1,7 @@
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends '' 
+    ? S
+    : S extends `${infer F}${From}${infer R}`
+       ? `${F}${To}${ReplaceAll<R, From, To>}`
+       : S
+  
