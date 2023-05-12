bd56bbbe9b197a069a6bb45a427133f76f5e8276
Grigory Korbut <grisga2001@gmail.com>
2023-05-12 09:33:25 +0300

    Create solution.ts

---
+++ medium/fibonacci-sequence/solution.ts
@@ -0,0 +1,4 @@
+type Fibonacci<T extends number, CurrentIndex extends any[] = [1], Prev extends any[] = [], Current extends any[] = [1]> = CurrentIndex['length'] extends T
+  ? Current['length']
+  : Fibonacci<T, [...CurrentIndex, 1], Current, [...Prev, ...Current]>
+  
