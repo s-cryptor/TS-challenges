e27cdd3311efc09474ef7e3226842b21c9b2b0e2
Grigory Korbut <grisga2001@gmail.com>
2023-05-16 08:50:21 +0300

    Create solution.ts

---
+++ medium/join/solution.ts
@@ -0,0 +1,6 @@
+type Join<T extends any[], U extends string | number> = T extends [infer F, ...infer R]
+  ? R["length"] extends 0
+    ? `${F & string}`
+    : `${F & string}${U}${Join<R, U>}`
+  : never
+  
