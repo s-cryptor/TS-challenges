7a64601807ca63e79a5f08e5040101c688e38e34
Grigory Korbut <grisga2001@gmail.com>
2023-05-16 08:54:13 +0300

    Create solution.ts

---
+++ medium/index-of/solution.ts
@@ -0,0 +1,9 @@
+type IsEqual<T, U> = U extends T ? T extends U ? true : false : false
+type IndexOf<T extends any[], U, Count extends any[] = []> = T extends [infer L, ...infer R]
+  ? IsEqual<L, U> extends true
+    ? Count['length']
+    : Count['length'] extends T['length']
+      ? -1
+      : IndexOf<R, U, [...Count, 0]>
+  : -1
+  
