a699872445df54cf2d2b399268a71c56b435aec2
Grigory Korbut <grisga2001@gmail.com>
2023-05-16 09:10:43 +0300

    Create solution.ts

---
+++ medium/without/solution.ts
@@ -0,0 +1,8 @@
+type ToUnion<T> = T extends any[] ? T[number] : T
+type Without<T, U> = 
+  T extends [infer F, ...infer R]
+    ? F extends ToUnion<U>
+      ? Without<R, U>
+      : [F, ...Without<R, U>]
+    : T
+  
